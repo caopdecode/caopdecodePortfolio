d4133d4dc27a3088a24e3a3f99f6578cfd2853ae
Camilo <caop.decode@gmail.com>
2025-01-17 11:29:51 -0500

    Portfolio CaopDecode InterfacesAdded

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import './App.css'
 import Switch, { switchClasses } from '@mui/joy/Switch';
 import Typography from '@mui/joy/Typography';
+import SWEPic from './IMGs/SWEPic.jpg';
 
 const Header = styled.div`
   display: flex;
@@ -35,7 +36,8 @@ const InfoCont = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: left;
-  background-color: #63E6FA;
+  background: rgb(4,189,218);
+  background: linear-gradient(152deg, rgba(4,189,218,1) 0%, rgba(99,230,250,1) 100%);
   width: 75%;
   height: 100%;
   border-radius: 50px;
@@ -55,7 +57,7 @@ const CardsContainer = styled.div`
   height: 100%;
 `;
 
-const Card = styled.div`
+const Card = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -65,13 +67,109 @@ const Card = styled.div`
   margin: 10px;
   border-radius: 20px;
   cursor: pointer;
+  transition: 0.5s;
+  &:hover{
+    background-color: rgba(4,189,218,1);
+    transition: 0.5s;
+  }
+`;
+
+const SWETitle = styled.h1`
+  color: #171717;
+  font-size: 50px;
+  font-style: italic;
+  margin: 0px;
+  margin-top: 30px;
+`;
+
+const ProfileTitle = styled.h2`
+  color: #171717;
+  font-style: italic;
+  margin: 0px;
+`;
+
+const SWEInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  height: 70%;
+  margin-top: 10px;
+`;
+
+const SWETextCont = styled.div`
+  display: flex;
+  width: 48%;
+  height: 100%;
+  background-color: #F5F5F5;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+`;
+
+const SWEPicCont = styled.div`
+  display: flex;
+  width: 48%;
+  height: 100%;
+  background-image: url(${SWEPic});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+`;
+
+const SWETextInfo = styled.p`
+  text-align: justify;
+  width: 90%;
+  height: 90%;
+  color: #171717;
 `;
 
 const CardText = styled.h2`
   color: #171717;
   font-size: 35px;
   cursor: pointer;
+  font-weight: 300;
 `;
+
+const SkillCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15%;
+  height: 90%;
+  background-color: #F5F5F5;
+  border-radius: 20px;
+  transition: 0.5s;
+  &:hover{
+    translate: 0px -10px;
+    box-shadow: 0px 20px 10px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+  }
+`;
+
+const SkillsSec = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 95%;
+  height: 50%;
+  margin: 20px 0px;
+`;
+
+const SkillsDivider = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 95%;
+`;
+
+
 
 function App() {
 
@@ -80,7 +178,28 @@ function App() {
   const handleToggleCheck = (e) =>{
     setChecked(e.target.checked);
   }
-  
+
+  const [about, setAbout] = useState(true);
+  const [skills, setSkills] = useState(false);
+  const [projects, setProjects] = useState(false);
+
+  const handleAbout = () =>{
+    setAbout(true);
+    setSkills(false);
+    setProjects(false);
+  }
+
+  const handleSkills = () =>{
+    setSkills(true);
+    setAbout(false);
+    setProjects(false);
+  }
+
+  const handleProjects = () =>{
+    setProjects(true);
+    setAbout(false);
+    setSkills(false);
+  }
 
   return (
     <>
@@ -127,18 +246,68 @@ function App() {
           <SectionsTitle>
             Sections
           </SectionsTitle>
-          <Card>
+          <Card onClick={handleAbout}>
             <CardText>About</CardText>
           </Card>
-          <Card>
+          <Card onClick={handleSkills}>
             <CardText>Skills</CardText>
           </Card>
-          <Card>
+          <Card onClick={handleProjects}>
             <CardText>Projects</CardText>
           </Card>
         </CardsContainer>
         <InfoCont>
+          <SWETitle>SOFTWARE ENGINEER</SWETitle>
+          <ProfileTitle>Profile</ProfileTitle>
+          <SWEInfo>
+          {about &&(
+            <>
+            <SWETextCont>
+              
+              <SWETextInfo>
+              As a passionate Software Engineer with 2-3 years of experience, I specialize in backend development, leveraging robust expertise in Java, SQL, Python, and PHP to deliver efficient and scalable solutions. My skills extend to frontend technologies, including HTML, CSS, JavaScript, and React.
+              <br/><br/>
+              I am a strong communicator, a self-driven learner, and an effective team player. My time management skills and commitment to problem-solving ensure that I deliver high-quality results under stablished and tight deadlines. My ultimate goal is to craft impactful software that meets user needs and drives innovation in every project I undertake.
+              </SWETextInfo>
+            </SWETextCont>
+            <SWEPicCont/>
+            </>
+          )}
+           {skills &&(
+            <>
+            <SkillsDivider>
+              <SkillsSec>
+              <SkillCard>
 
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              </SkillsSec>
+              <SkillsSec>
+              <SkillCard>
+                
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              <SkillCard>
+                
+              </SkillCard>
+              </SkillsSec>
+            </SkillsDivider>
+            </>
+          )}
+          </SWEInfo>
         </InfoCont>
       </InfoBackgr>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@fontsource/lato'
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
+import '@fontsource/lato/700-italic.css';
+import '@fontsource/lato/900-italic.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
